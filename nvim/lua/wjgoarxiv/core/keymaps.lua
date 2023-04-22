b803c66496bb6bfb579606b3ac855c845d59c511
wjgoarxiv <118244317+wjgoarxiv@users.noreply.github.com>
2023-04-22 23:04:10 +0900

    Update keymaps.lua

--- nvim/lua/wjgoarxiv/core/keymaps.lua
+++ nvim/lua/wjgoarxiv/core/keymaps.lua
@@ -27,3 +27,15 @@ keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in 
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tag
+
+-- Move the current line up
+vim.api.nvim_set_keymap("n", "<A-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
+
+-- Move the current line down
+vim.api.nvim_set_keymap("n", "<A-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
+
+-- Copy the current line and paste it above the current line
+vim.api.nvim_set_keymap("n", "<S-A-Up>", "YpkP", { noremap = true, silent = true })
+
+-- Copy the current line and paste it below the current line
+vim.api.nvim_set_keymap("n", "<S-A-Down>", "Yp", { noremap = true, silent = true })
