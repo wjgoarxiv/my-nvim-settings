b354b870fa8ef489e03e66b4ffa1f812065947f8
wjgoarxiv <118244317+wjgoarxiv@users.noreply.github.com>
2024-02-06 17:46:28 +0900

    Modified `plugins-setup.lua` and `lualine.lua`

--- nvim/lua/wjgoarxiv/plugins/lualine.lua
+++ nvim/lua/wjgoarxiv/plugins/lualine.lua
@@ -1,11 +1,31 @@
 local status, lualine = pcall(require, "lualine")
 if not status then
-    return
+	return
 end
 
-lualine.setup {
-    options = {
-        theme = 'auto', -- Use 'auto' for default theme or set to 'gruvbox' if already installed
-    }
-    -- other default configurations
+local lualine_tokyonight = require("lualine.themes.tokyonight")
+
+local new_colors = {
+	blue = "#5E81AC", -- nord10
+	green = "#A3BE8C", -- nord14
+	violet = "#B48EAD", -- nord15
+	yellow = "#EBCB8B", -- nord13
+	black = "#3B4252", -- nord1
 }
+
+lualine_tokyonight.normal.a.bg = new_colors.blue
+lualine_tokyonight.insert.a.bg = new_colors.green
+lualine_tokyonight.visual.a.bg = new_colors.violet
+lualine_tokyonight.command = {
+	a = {
+		gui = "bold",
+		bg = new_colors.yellow,
+		fg = new_colors.black,
+	},
+}
+
+lualine.setup({
+	options = {
+		theme = lualine_tokyonight,
+	},
+})
